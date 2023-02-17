f2244314ebff52a22a6b23f9ec49985fca519d97
kim <kimhm0918@naver.com>
2023-02-18 01:44:38 +0900

    20230218

--- js/gameMenu.js
+++ js/gameMenu.js
@@ -1,11 +1,9 @@
 const gameMenu = (ruletGame)=>{
   const wrap = document.querySelector('.wrap');
-  const menuWrap = document.createElement('div');
   const rulet = document.createElement('button');
   rulet.innerText = '룰렛게임';
   rulet.addEventListener('click',(e)=>ruletGame(e,gameMenu));
-  menuWrap.append(rulet);
-  wrap.append(menuWrap);
+  wrap.append(rulet);
 }
 
 export default gameMenu
